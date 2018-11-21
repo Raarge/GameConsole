b76f0ba1ad4b0562a200833df25b324e7be1b825
Campbell, William (164) <wicampb@americas.corpdir.net>
2018-11-21 16:11:28 -0500

    added classes and nullable of t for value types

---
+++ GameConsole/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+        public int? DaysSinceLastLogin { get; set; }  //short hand int?  instead of Nullable<int>
+        public DateTime? DateOfBirth { get; set; }  // same as above
+
+        public PlayerCharacter()
+        {
+            DateOfBirth = null;
+            DaysSinceLastLogin = null;
+
+            // Using Magic Numbers Not the Preferred Way to Do this
+            //DateOfBirth = DateTime.MinValue;  //magic number
+            //DaysSinceLastLogin = -1; // magic number
+        }
+    }
+}
